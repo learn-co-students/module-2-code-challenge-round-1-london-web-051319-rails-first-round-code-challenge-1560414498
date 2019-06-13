250c363395e4f2e5b7cf9fa94be2939e9c87a14a
Alex Johnston <alexjohnston@ymail.com>
2019-06-13 10:04:15 +0100

    made full MVP

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -6,4 +6,23 @@ class HeroinesController < ApplicationController
   def show
     @heroine = Heroine.find params[:id]
   end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    heroine = Heroine.create heroine_params
+    if heroine.valid?
+      redirect_to heroine
+    else
+      flash[:errors] = heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
